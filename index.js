72ad9c52aedd945357cdd8828d6cdf7736fbc5fb
phxtung <tungpx@symper.vn>
2021-12-21 00:18:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function openSidebar() {  
+    document.getElementById("sidebar").style.left = "0";
+    // document.getElementById("sidebar").style.width = "70%";
+}
+function closeSidebar() {  
+    document.getElementById("sidebar").style.left = "-1000px";
+    // document.getElementById("sidebar").style.width = "0%";
+}
